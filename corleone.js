9127f5585a2fabb914c9cd6472d1a51af944ec65
unknown <matthieu@webvalue.nl>
2016-03-21 20:31:40 +0100

    Reformated prototype, added attribute data-don-inject and changed data-don to data-don-event

--- corleone.js
+++ corleone.js
@@ -10,41 +10,74 @@ Corleone.prototype = {
 	ready : function(){
 		if(this.readyEvent){
 			var elements = (this.elements.length == 1 ? this.elements[0] : this.elements);
-			this.readyEvent.bind(this.state)(elements);
+			this.readyEvent.bind(Object.assign(this.state, this.methods))(elements);
 		}
 	},
-	build : function(){
+	addEvents : function(){
 		// Apply event handlers
-		for (var i = 0; i < this.elements.length; i++) {
-			if(this.events){
+		if(this.events){
+			for (var i = 0; i < this.elements.length; i++) {
 				for(eventName in this.events){
 					this.cache[eventName] = function(e){
 						this.proto.events[eventName].bind(Object.assign(this.proto.state, this.proto.methods))(e, this.element);
 					};
 					this.elements[i].addEventListener(eventName, this.cache[eventName].bind({proto : this, element : this.elements[i]}), false);
 				}
-			}
-		};
+			};
+		}
+	},
+	addMethods : function(){
 		// Bind custom methods
 		if(this.methods){
-			var foundDonElements = [];
-			for (var i = 0; i < this.elements.length; i++) {
-				var query = this.elements[i].querySelectorAll('[data-don]');
-				query = Array.prototype.slice.call(query);
-				foundDonElements = foundDonElements.concat(query);
+			var foundDon = {
+				events : [],
+				injects : []
 			};
-			foundDonElements.forEach(function(element, index){
-				var donAction = element.getAttribute('data-don').split(':');
-				element.addEventListener(donAction[0], this.methods[donAction[1]], false);
-			}.bind(this));
+			var getDonDOM = function(element, selector, dump){
+				var query = element.querySelectorAll(selector);
+				query = Array.prototype.slice.call(query);
+				foundDon[dump] = foundDon[dump].concat(query);
+			}
+			// Collect don elements
+			for (var i = this.elements.length - 1; i >= 0; i--) {
+				getDonDOM(this.elements[i], '*[data-don-event]', 'events');
+				getDonDOM(this.elements[i], '*[data-don-inject]', 'injects');
+			};
+			// Handle event types
+			for (var x = foundDon.events.length - 1; x >= 0; x--) {
+				var attribute = foundDon.events[x].getAttribute('data-don-event').replace(/ /g,'');
+				var events = attribute.split(',');
+				for (var i = events.length - 1; i >= 0; i--) {
+					var donEvent = events[i].split(':');
+					foundDon.events[x].addEventListener(donEvent[0], this.methods[donEvent[1]], false);
+				};
+			};
+			// Handle injection types
+			for (var x = foundDon.injects.length - 1; x >= 0; x--) {
+				var attribute = foundDon.events[x].getAttribute('data-don-inject').replace(/ /g,'');
+				var injects = attribute.split(',');
+				for (var i = injects.length - 1; i >= 0; i--) {
+					var donInject = injects[i].split(':');
+					switch(donInject[0]){
+						case 'text' :
+							foundDon.injects[x].textContent = this.state[donInject[1]];
+						break;
+
+						default :
+							foundDon.injects[x].setAttribute(donInject[0], donInject[1]);
+					}
+				};
+			};
 		}
 	}
 }
+
 var Don = function(selector, data){
 	var performance = new Performance('Don Exec for '+selector, 3);
 
 	var corleone = new Corleone(selector, data);
-	corleone.build();
+	corleone.addEvents();
+	corleone.addMethods();
 	corleone.ready();
 
 	performance.measure();
